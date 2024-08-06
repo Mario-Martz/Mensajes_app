89b663d319a32c3e3c44c765c90fbe88b9db690e
Mario Martinez <mariomejiamartinez08@gmail.com>
2024-08-06 16:16:02 -0600

    Configurazion de las variables, ya nos conecta a la base de datos

--- src/main/java/com/platzi/mensajes_app/Conexion.java
+++ src/main/java/com/platzi/mensajes_app/Conexion.java
@@ -6,10 +6,10 @@ import java.sql.SQLException;
 
 public class Conexion {
     //Variables staticas 
-    private static final String USER = "";
-    private static final String PASS = "";
-    private static  final String DB = "";
-    private static  final String URL = "";
+    private static final String USER = "postgres";
+    private static final String PASS = "postgres";
+    private static final String DB = "DB_Peliculas";
+    private static final String URL = "jdbc:postgresql://127.0.0.1:5432/" + DB;
    
     public static Connection getConexion() throws SQLException{
         return DriverManager.getConnection(URL, USER, PASS);
--- src/main/java/com/platzi/mensajes_app/Inicio.java
+++ src/main/java/com/platzi/mensajes_app/Inicio.java
@@ -1,6 +1,14 @@
 package com.platzi.mensajes_app;
+
+import java.sql.SQLException;
+
 public class Inicio {
     public static void main(String[] args) {
-        
+        try {
+            Conexion.getConexion();
+            System.out.println("Genial nos conectamos");
+        } catch (SQLException ex) {
+            ex.printStackTrace(System.out);
+        }
     }
 }
